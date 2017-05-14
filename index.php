6d08abaed77836eecaf55558907d76b014400c50
lkfb <lkfb@hushmail.com>
2017-05-14 00:01:21 +0000

    iss
    iss

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php
 function isMobile() {
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
@@ -7,3 +8,4 @@ if(isMobile()){
 else {
     header("location: r.php");
 }
+?>
